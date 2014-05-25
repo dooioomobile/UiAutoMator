e94b196b816f8409261cf98b28e75b8cfffbaac9
cong <cong.chang@gmail.com>
2014-05-25 18:32:55 +0800

    begin to add friends

--- src/com/Runner.java
+++ src/com/Runner.java
@@ -1,4 +1,5 @@
 package com;  
+import com.android.uiautomator.core.UiDevice;
 import com.android.uiautomator.core.UiObject;  
 import com.android.uiautomator.core.UiObjectNotFoundException;  
 import com.android.uiautomator.core.UiScrollable;  
@@ -7,18 +8,87 @@ import com.android.uiautomator.testrunner.UiAutomatorTestCase;
   
 public class Runner extends UiAutomatorTestCase {  
   
+	private final static int SleepTime = 200;
     public void testDemo() throws UiObjectNotFoundException {  
-        getUiDevice().pressHome();  
-        // 进入设置菜单  
-        UiObject settingApp = new UiObject(new UiSelector().text("微信"));  
-        settingApp.click();  
-        //休眠3秒  
+        UiDevice device = getUiDevice();
+        device.pressHome();
+        
+        //UiObject wx = new UiObject(new UiSelector().packageName("com.tencent.mm"));
+        //wx.click();
+        
         try {  
-            Thread.sleep(3000);  
+            Thread.sleep(SleepTime);  
         } catch (InterruptedException e1) {  
             // TODO Auto-generated catch block  
             e1.printStackTrace();  
         }  
+    
+        UiObject settingApp = new UiObject(new UiSelector().text("微信"));  
+        settingApp.click();  
+        //休眠3秒  
+        try {  
+            Thread.sleep(SleepTime);  
+        } catch (InterruptedException e1) {  
+            // TODO Auto-generated catch block  
+            e1.printStackTrace();  
+        }  
+        
+        // click "+" image view
+        device.click(552, 98);
+        try {  
+            Thread.sleep(SleepTime);  
+        } catch (InterruptedException e1) {  
+            // TODO Auto-generated catch block  
+            e1.printStackTrace();  
+        }  
+        
+        // click "add friends"
+        device.click(512, 289);
+        try {  
+            Thread.sleep(SleepTime);  
+        } catch (InterruptedException e1) {  
+            // TODO Auto-generated catch block  
+            e1.printStackTrace();  
+        }  
+        
+        //input phonenumber now
+        UiObject phone = new UiObject(new UiSelector().className("android.widget.EditText"));
+        phone.setText("13681902026");
+        device.pressEnter();
+        
+        //click search button
+        UiObject confirm = new UiObject(new UiSelector().className("android.widget.Button"));
+        confirm.clickAndWaitForNewWindow();
+        
+        //click "add to contact"
+        UiObject add = new UiObject(new UiSelector().className("android.widget.Button"));
+        add.clickAndWaitForNewWindow();
+        
+        UiObject send = new UiObject(new UiSelector().text("发送"));
+        send.clickAndWaitForNewWindow();
+        
+        //now back
+
+        device.pressBack();
+        
+        device.pressBack();
+        
+        device.pressBack();
+        /*
+        UiObject pswd = new UiObject(new UiSelector().className("android.widget.EditText"));
+        pswd.setText("2411domy");
+        getUiDevice().pressEnter();
+        try {  
+            Thread.sleep(2000);  
+        } catch (InterruptedException e1) {  
+            // TODO Auto-generated catch block  
+            e1.printStackTrace();  
+        }  
+        
+        //UiObject login = new UiObject(new UiSelector().text("登陆"));
+
+        //login.click();
+        getUiDevice().click(40, 760);
         /*
         // 进入语言和输入法设置  
         UiScrollable settingItems = new UiScrollable( new UiSelector().scrollable(true));  
